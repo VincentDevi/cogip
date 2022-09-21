bd09f472fe101234f99c9ee03a519eb90f535a25
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-09-21 14:33:31 +0200

    Add dbSettings file to connect to database without dig in code.

--- models/Dbh.php
+++ models/Dbh.php
@@ -5,18 +5,20 @@ namespace App\models;
 use PDO;
 use PDOException;
 
+require_once './dbSettings.php';
+
 /**
  * Connect to database.
  */
 class Dbh
 {
-    private string $user = "vinz";
-    private string $password = "5739";
+//    private string $user = "vinz";
+//    private string $password = "5739";
 
     protected function connexion()
     {
         try {
-            return  new PDO("mysql:host=localhost;dbname=database", $this->user, $this->password);
+            return  new PDO("mysql:host=".HOST.";dbname=".DATABASE.";port=".PORT, USER, PASSWORD);
         }catch (PDOException $e){
             echo "Error: ".$e->getMessage();
             die();
