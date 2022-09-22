6d0b58570623f18416d8782f5e93d1c948bd0543
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-09-22 14:24:25 +0200

    Add default user as Jean christian

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/Core/Helper.php';
+require_once __DIR__.'/models/globalSettings.php';
 require_once __DIR__.'/Routes/Routes.php';
 
