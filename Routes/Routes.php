af1ad9bb1ae6794f7eb863deb2efe1e600be9350
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-10-03 16:06:07 +0200

    Clean Routes.php

--- Routes/Routes.php
+++ Routes/Routes.php
@@ -10,15 +10,9 @@ use App\Views\InvoiceView;
 use App\Views\NotFoundView;
 use Bramus\Router\Router;
 use App\Controllers\HomeController;
-use App\Controllers\NotFoundController;
-use App\Controllers\ContactsController;
-use App\Controllers\InvoicesController;
-use App\Controllers\CompanyController;
-use App\Controllers\ContactController;
 
 use App\Test\ValidateUserInputTest;
 
-use App\models\DbData;
 
 $router = new Router();
 
@@ -31,47 +25,31 @@ $router->set404(function() {
 });
 
 $router->get('/', function() {
-//   echo 'home';
- (new HomeController)->index();
+    (new HomeController)->index();
 });
 
 $router->get('/companies', function() {
     (new CompanyViews)->showAll();
- //   echo 'companies';
 });
 
 $router->get('/contacts', function() {
-////    echo 'contacts';
     (new ContactViews())->showAll();
 });
 
 $router->get('/invoices', function() {
-//    echo 'invoices';
     (new InvoiceView())->showAll();
 });
 
 $router->get('/test', function() {
-//    echo 'invoices';
     (new validateUserInputTest());
 });
 
-//$router->get('/contact', function() {
-////    echo 'contacts';
-//    (new ContactController())->index();
-//});
 $router->get('/contact/([0-9]+)', function($name) {
-    // get data's from DB here and pass it to index function
-
     (new ContactViews())->show($name);
 });
-//$router->get('company', function() {
-  //  (new CompanyController())->index();
-    //   echo 'companies';
-//});
-$router->get('/company/([0-9]+)', function($name) {
-    // get data's from DB here and pass it to index function
 
-        (new CompanyViews())->show($name);
+$router->get('/company/([0-9]+)', function($name) {
+    (new CompanyViews())->show($name);
 });
 
 $router->put('/create/invoice/reference/price/company', function($reference, $price, $company) {
