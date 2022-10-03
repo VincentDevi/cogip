e7cfd3ed4959185cca477ef8ac0b7d0e87c61ef5
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-10-03 15:48:12 +0200

    Clean ContactController.php

--- Controllers/ContactController.php
+++ Controllers/ContactController.php
@@ -3,23 +3,10 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
-use App\models\CompanyData;
 use App\models\contactData;
+
 class ContactController extends Controller
 {
-//    /**
-//     * return view
-//     *
-//     * @param $data
-//     * @return void
-//     */
-//    public function index($contact)
-//    {
-//        $data = new contactData();
-//        $datas = $data->getcontactData($contact);
-//        $this->view('contact', $datas);
-//    }
-
     public function create($data) {
 
     }
