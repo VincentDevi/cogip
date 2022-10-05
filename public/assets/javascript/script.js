8fd23a2b792ddcdd32dccd444e8b94aeae73f926
Xavier Brigode <xavier.brigode@gmail.com>
2022-10-05 10:06:12 +0200

    add lateral picture on homepage, complete footer responsive, fix navbar responsive behavior

--- public/assets/javascript/script.js
+++ public/assets/javascript/script.js
@@ -1,8 +1,8 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "header__menu__nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "header__menu__nav";
-    }
-  } 
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "header__menu__nav") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "header__menu__nav";
+//     }
+//   } 
